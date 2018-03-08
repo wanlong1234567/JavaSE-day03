ff500914edd73ae30bbddbd9b23df474ee27d533
wanlong1234567 <2531165965@qq.com>
2018-03-08 16:14:35 +0800

    关于Claendar的set方法

---
+++ day03_all/day03/Calendar_set.java
@@ -0,0 +1,48 @@
+package day03;
+
+import java.util.Calendar;
+
+/**
+ * void set(int field,int value)
+ * 对指定的时间分量设置指定的值
+ * @author adminitartor
+ *
+ */
+public class Calendar_set {
+	public static void main(String[] args) {
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar.getTime());
+		/*
+		 * 设置为2008-08-08 20:08:08
+		 */
+		//设置年
+		calendar.set(Calendar.YEAR, 2008);
+		calendar.set(Calendar.MONTH,Calendar.AUGUST);
+		calendar.set(Calendar.DAY_OF_MONTH,8);
+		calendar.set(Calendar.HOUR_OF_DAY,20);
+		calendar.set(Calendar.MINUTE,8);
+		calendar.set(Calendar.SECOND,8);
+		System.out.println(calendar.getTime());
+		/*
+		 * 设置某些时间分量的值可能导致其他时间
+		 * 分量值的变化
+		 * 例如:设置了周几后，对应的日期也会改变
+		 */
+		calendar.set(Calendar.DAY_OF_WEEK,4);
+		System.out.println(calendar.getTime());
+		/*
+		 * 可以设置时间分量对应的值超过其范围，
+		 * Calendar会自动进位。
+		 */
+		calendar.set(Calendar.DAY_OF_MONTH,32);
+		System.out.println(calendar.getTime());
+	}
+}
+
+
+
+
+
+
+
+
